3c0bd88879e0c14e7a9768b210605f19fa7ff1f0
salah-99 <57250862+salah-99@users.noreply.github.com>
2021-04-13 15:33:37 +0000

    Update OrderScreen.js

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -34,7 +34,7 @@ export default function OrderScreen(props) {
   const dispatch = useDispatch();
   useEffect(() => {
     const addPayPalScript = async () => {
-      const { data } = await Axios.get('/api/config/paypal');
+      const { data } = await Axios.get('https://frontdrogstore.herokuapp.com/api/config/paypal');
       const script = document.createElement('script');
       script.type = 'text/javascript';
       script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
